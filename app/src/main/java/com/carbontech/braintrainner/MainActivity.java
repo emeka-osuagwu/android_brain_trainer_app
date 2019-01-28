253db63792b6d893ae726f7726d66c5221013d8f
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2019-01-28 12:02:17 +0100

    add ui layout

--- app/src/main/java/com/carbontech/braintrainner/MainActivity.java
+++ app/src/main/java/com/carbontech/braintrainner/MainActivity.java
@@ -2,12 +2,23 @@ package com.carbontech.braintrainner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button start_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start_button = (Button) findViewById(R.id.start_button);
+    }
+
+    public void start(View view){
+        start_button.setVisibility(View.INVISIBLE);
     }
 }
+
